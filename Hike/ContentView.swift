ddeb62f78a77edeb861e4f747fbad41a3e7e98a7
신동규 <donggyu9410@gmail.com>
2024-04-06 12:50:23 +0900

    set up project

---
+++ Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by 신동규 on 4/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 신동규 on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
